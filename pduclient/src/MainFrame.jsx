359644344e90139ce2dbf021341ed4efb4a3bd5a
sachuuuu <sachithasa@gmail.com>
2018-06-15 14:10:04 +0530

    Done

--- pduclient/src/MainFrame.jsx
+++ pduclient/src/MainFrame.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './PatientContainer.css';
 import PatientContainer from "./PatientContainer";
